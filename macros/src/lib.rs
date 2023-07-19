458750295a34e50ae94d620e5c7e635f9f1e4776
zancas <zancas@zingolabs.org>
2023-07-19 15:35:05 -0600

    use modern syn syntax

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,10 +2,14 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
 pub fn duration_annotation_test(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let function = parse_macro_input!(input as ItemFn);
+    let function = if let Item::Fn(funct) = parse_macro_input!(input as Item) {
+        funct
+    } else {
+        panic!("Arrgggh!")
+    };
     TokenStream::from(quote! {#function})
 }
