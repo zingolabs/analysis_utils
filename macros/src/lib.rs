ad698b92030e99bb8901fd18d4baf69708ab0da9
zancas <zancas@zingolabs.org>
2023-07-19 15:29:29 -0600

    defer arg parsing until later

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,11 +2,10 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, ItemFn};
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
-pub fn duration_annotation_test(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args = parse_macro_input!(args as AttributeArgs);
+pub fn duration_annotation_test(_args: TokenStream, input: TokenStream) -> TokenStream {
     let function = parse_macro_input!(input as ItemFn);
     TokenStream::from(quote! {#function})
 }
