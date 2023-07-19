6f689ab6d981eb52f4a60d8008fc334474137986
zancas <zancas@zingolabs.org>
2023-07-19 15:09:04 -0600

    add boilerplate to proc_macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
+use quote::quote;
+use syn::{parse_macro_input, AttributeArgs, ItemFn};
 
 #[proc_macro_attribute]
 pub fn duration_annotation_test(args: TokenStream, input: TokenStream) -> TokenStream {
-    todo!();
+    let args = parse_macro_input!(args as AttributeArgs);
+    let function = parse_macro_input!(input as ItemFn);
+    TokenStream::from(quote! {#function})
 }
