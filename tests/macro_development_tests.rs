c4ed4d906c9120d4c27ff6faf4c49de70841e818
zancas <zancas@zingolabs.org>
2023-08-03 18:29:01 -0600

    first attribute is expected to be the relevant scenario

--- tests/macro_development_tests.rs
+++ tests/macro_development_tests.rs
@@ -3,7 +3,7 @@ use macros::annotated_benchmark;
 use std::time::Instant;
 use zingo_testutils::scenarios;
 
-#[annotated_benchmark]
+#[annotated_benchmark(unsynced_faucet_recipient_1153)]
 async fn basic_annotated_benchmark_test() {
     println!("HELLO");
 }
