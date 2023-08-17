b0fce747b77d51688e9b349855a1118827527843
zancas <zancas@zingolabs.org>
2023-08-17 07:48:22 -0600

    fix imports

--- src/scenarios.rs
+++ src/scenarios.rs
@@ -1,9 +1,8 @@
-use zingo_testutils::build_fvk_client_and_capability;
-use zingo_testutils::build_fvks_from_wallet_capability;
 use zingo_testutils::data::seeds::HOSPITAL_MUSEUM_SEED;
 use zingo_testutils::regtest::ChildProcessHandler;
 use zingo_testutils::regtest::RegtestManager;
 use zingo_testutils::scenarios::setup::ScenarioBuilder;
+use zingo_testutils::{build_fvk_client, build_fvks_from_wallet_capability};
 use zingolib::lightclient::LightClient;
 pub async fn unsynced_viewonlyclient_1153() -> (
     RegtestManager,
@@ -22,17 +21,11 @@ pub async fn unsynced_viewonlyclient_1153() -> (
     // Create a lightclient to extract a capability from.
     let original_recipient = sb.client_builder.build_new_faucet(0, false).await;
     // Extract viewing keys
-    let wallet_capability = original_recipient
-        .wallet
-        .wallet_capability()
-        .read()
-        .await
-        .clone();
+    let wallet_capability = original_recipient.wallet.wallet_capability().clone();
     // Delete the client after getting the capability.
     // Extract the orchard fvk
     let [o_fvk, s_fvk, t_fvk] = build_fvks_from_wallet_capability(&wallet_capability);
-    let (viewing_client, _) =
-        build_fvk_client_and_capability(&[&o_fvk, &s_fvk, &t_fvk], &zingo_config).await;
+    let viewing_client = build_fvk_client(&[&o_fvk, &s_fvk, &t_fvk], &zingo_config).await;
     (
         sb.regtest_manager,
         sb.child_process_handler.unwrap(),
